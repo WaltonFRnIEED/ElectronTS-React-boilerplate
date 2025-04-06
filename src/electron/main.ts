9df0d93bf52e7ad5849720dedfde79c119edab48
Fahad <fhd.walton@gmail.com>
2025-04-06 16:06:46 +0600

    added dotenv

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,6 +1,9 @@
 import { app, BrowserWindow } from 'electron'
 import path from 'path'
+import dotenv from 'dotenv';
+dotenv.config();
 import { isDev } from './utils.js';
+
 
 app.whenReady().then(() => {
     const mainWindow = new BrowserWindow({})
